4a0d75e9ffb114af6d0f067657d31095d7189150
Junfeng ou <junfengou@gmail.com>
2020-08-30 16:28:50 -0500

    first commit

---
+++ src/Components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div>
+      <h3>This is the banner</h3>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Banner from "./Banner";
+
+function Home() {
+  return (
+    <div className="home">
+      <h3>This is the home page</h3>
+      <Banner />
+    </div>
+  );
+}
+
+export default Home;
